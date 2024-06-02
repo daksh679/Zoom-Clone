213bad11bdef82c3e84eb2569379a3c8cb9a16f8
Daksh Ahuja <adaksh679@gmail.com>
2024-06-02 20:52:29 +0530

    first commit

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+const MeetingTypeList = () => {
+  return (
+    <section className="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-4">
+      <div
+        className="bg-orange-1 flex flex-col justify-between items-center  px-4 py-6 w-full rounded-[14px] min-h-[260px] xl:max-w-[270px]"
+        onClick={() => {}}
+      >
+        Box 1
+      </div>
+    </section>
+  );
+};
+
+export default MeetingTypeList;
