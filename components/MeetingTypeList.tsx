e552265d1993f5d2dd0843978713647b2a4e7b68
Daksh Ahuja <adaksh679@gmail.com>
2024-06-09 20:06:03 +0530

    first commit

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,14 +3,69 @@
 import { useState } from "react";
 import Homecard from "./Homecard";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import { useToast } from "./ui/use-toast";
 
 // import Image from "next/image";
 
 const MeetingTypeList = () => {
+  const { toast } = useToast();
+  const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isJoiningMeeting" | "isScheduleMetting" | "isInstantMeeting" | undefined
   >();
-  const createMeeting = () => {};
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+  const [callDetails, setCallDetails] = useState<Call>();
+  const createMeeting = async () => {
+    if (!client || !user) return;
+
+    try {
+      if (!values.dateTime) {
+        toast({
+          title: "Please select a time and date",
+        });
+        return;
+      }
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to create call");
+
+      const startAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Instant Meeting";
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startAt,
+          custom: {
+            description: description,
+          },
+        },
+      });
+
+      setCallDetails(call);
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      toast({
+        title: "Meeting Created",
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "failed to create a meeting",
+      });
+    }
+  };
 
   return (
     <section className="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-4">
