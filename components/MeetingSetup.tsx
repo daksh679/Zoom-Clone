1c4d240dd3d62ddc618b1bd35de6e4ba8826ef86
Daksh Ahuja <adaksh679@gmail.com>
2024-06-16 15:02:20 +0530

    first commit

---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,45 @@
+"use client";
+import {
+  DeviceSettings,
+  VideoPreview,
+  useCall,
+} from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
+
+const MeetingSetup = () => {
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
+  const call = useCall();
+
+  if (!call) {
+    throw new Error("usecall must be used within stremcall component");
+  }
+
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToggledOn, call?.microphone, call?.camera]);
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-3xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn}
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        <DeviceSettings />
+      </div>
+    </div>
+  );
+};
+
+export default MeetingSetup;
